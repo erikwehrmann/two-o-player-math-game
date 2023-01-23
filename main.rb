e1b76f2f1e3dd427e577780ea7bb7efb8ac51818
erikwehrmann <erik.wehrmann@gmail.com>
2023-01-23 18:20:16 +0000

    final commit

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ player2 = Player.new("Player 2")
 
 game = Game.new(player1, player2)
 
-while player1.lives && player2.lives do
+while player1.lives > 0 && player2.lives > 0 do
   game.start_of_round
   game.access_guess
   game.end_of_round
